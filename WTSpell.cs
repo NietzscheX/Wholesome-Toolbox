cce4e1abff1eca4389105816ee5dfdd1c3deb372
Nico <jeanlouisledaron@gmail.com>
2022-05-06 15:18:59 +0200

    Added WTSpell.GetId

---
+++ WTSpell.cs
@@ -0,0 +1,27 @@
+﻿using wManager.Wow.Helpers;
+
+namespace WholesomeToolbox
+{
+    /// <summary>
+    /// Spell related methods
+    /// </summary>
+    public class WTSpell
+    {
+        /// <summary>
+        /// Returns the id of the max rank of the spell
+        /// </summary>
+        /// <returns>spell (max rank) id</returns>
+        public static uint GetId(string spellName, int rank)
+        {
+            string rankName = rank == 0 ? "" : $"Rank {rank}";
+            return Lua.LuaDoString<uint>($@"
+                local spellLink = GetSpellLink(""{spellName.EscapeLuaString()}"", ""{rankName}"");
+                if spellLink then
+                    local _, _, result = string.find(spellLink, ""Hspell:(%d+)|"");
+                    return result;
+                end
+                return 0;
+            ");
+        }
+    }
+}
