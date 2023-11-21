3cf46b3ec246748150f6a223044fc477d1f7d01f
libai <libai@libai.com>
2023-11-21 09:10:48 +0800

    init fly in toolbox

---
+++ WTTFly.cs
@@ -0,0 +1,36 @@
+﻿using robotManager.Helpful;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using wManager.Wow.Enums;
+using wManager.Wow.Helpers;
+using wManager.Wow.ObjectManager;
+using static wManager.Wow.Helpers.PathFinder;
+
+namespace WholesomeToolbox
+{
+
+    public class FlyHelper
+    {
+        public static void FlyTo(Vector3 pos)
+        {
+            int processId = (int)wManager.Wow.Memory.WowMemory.Memory.GetProcess().Id;
+            MemoryRobot.Memory memory = new MemoryRobot.Memory(processId);
+            uint BaseAddress = (uint)memory.ReadInt32(0xCD87A8);
+            BaseAddress = (uint)memory.ReadInt32(BaseAddress + 0x34);
+            BaseAddress = (uint)memory.ReadInt32(BaseAddress + 0x24);
+            //wManager.Wow.Memory.WowMemory.Memory.WriteByte(BaseAddress + 0x7CD,(byte)0x04);
+            memory.WriteFloat(BaseAddress + 0x798, pos.X);
+            memory.WriteFloat(BaseAddress + 0x79C, pos.Y);
+            memory.WriteFloat(BaseAddress + 0x7A0, pos.Z);
+
+            wManager.Wow.Helpers.Move.JumpOrAscend(wManager.Wow.Helpers.Move.MoveAction.PressKey, 100);
+
+            Thread.Sleep(3000);
+            //wManager.Wow.Memory.WowMemory.Memory.WriteByte(BaseAddress + 0x7CD, (byte)0x00);
+        }
+
+
+    }
+
+}
